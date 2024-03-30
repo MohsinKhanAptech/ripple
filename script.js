75857be8b70fb83908c34609cf9eee7be26c1884
Mohsin Khan <muhammadmohsinkhan.aptech@gmail.com>
2024-03-31 00:36:55 +0500

    added pause button animation stops when window is not in focus and other optimizations

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-let winHeight = window.innerHeight;
-let winWidth = window.innerWidth;
+// get  window width and height
+let winHeight, winWidth, running;
+let rippleContainer = document.querySelector(".rippleContainer");
 
+const resizeObserver = new ResizeObserver((entries) => {
+	winHeight = entries[0].target.clientHeight;
+	winWidth = entries[0].target.clientWidth;
+});
+
+resizeObserver.observe(document.body);
+
+// get random number
 function getRandomInt(min, max) {
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const setRandomInterval = (intervalFunction, minDelay, maxDelay) => {
+// execute function at random intervals
+function setRandomInterval(intervalFunction, minDelay, maxDelay) {
 	let timeout;
 
 	const runInterval = () => {
@@ -16,24 +26,24 @@ const setRandomInterval = (intervalFunction, minDelay, maxDelay) => {
 			runInterval();
 		};
 
-		const delay =
-			Math.floor(Math.random() * (maxDelay - minDelay + 1)) + minDelay;
-
+		const delay = getRandomInt(minDelay, maxDelay);
 		timeout = setTimeout(timeoutFunction, delay);
 	};
 
-	runInterval();
-
 	return {
-		clear() {
+		start() {
+			runInterval();
+			running = true;
+		},
+		stop() {
 			clearTimeout(timeout);
+			running = false;
 		},
 	};
-};
+}
 
-let body = document.querySelector("body");
-
-function placeElement() {
+// place ripples in ripple container
+function placeRippleNode() {
 	let o1 = document.createElement("div");
 	o1.classList.add("ripple");
 
@@ -44,12 +54,80 @@ function placeElement() {
 	o1.style.left = y + "px";
 	o1.style.animationDuration = getRandomInt(1000, 3000) + "ms";
 
-	body.appendChild(o1);
+	rippleContainer.appendChild(o1);
 }
 
-setRandomInterval(placeElement, 500, 1500);
-setInterval(() => {
-	if (body.childElementCount > 15) {
-		body.removeChild(body.firstChild);
-	}
-}, 500);
+// remove extra ripple objects from dom so its not cluttered
+function removeRippleNode() {
+	let timeout;
+
+	const runInterval = () => {
+		const timeoutFunction = () => {
+			if (rippleContainer.childElementCount > 10) {
+				rippleContainer.removeChild(rippleContainer.firstChild);
+			}
+			runInterval();
+		};
+		timeout = setTimeout(timeoutFunction, 500);
+	};
+	return {
+		start() {
+			runInterval();
+		},
+		stop() {
+			clearTimeout(timeout);
+		},
+	};
+}
+
+// start ripple remove function
+const removeRipple = setRandomInterval(removeRippleNode, 500, 500);
+removeRipple.start();
+
+// start ripple add function
+const addRipple = setRandomInterval(placeRippleNode, 500, 1500);
+addRipple.start();
+
+// function for toggle ripples
+let Ripples = {
+	toggle: function () {
+		if (running === true) {
+			addRipple.stop();
+			removeRipple.stop();
+		} else {
+			addRipple.start();
+			removeRipple.start();
+		}
+	},
+	// function for start ripples
+	start: function () {
+		if (running !== true) {
+			addRipple.start();
+			removeRipple.start();
+		}
+	},
+	// function for stop ripples
+	stop: function () {
+		if (running === true) {
+			addRipple.stop();
+			removeRipple.stop();
+		}
+	},
+};
+
+// toggles ripples on window focus so it doesn't keep running in the background
+window.addEventListener("blur", Ripples.stop);
+window.addEventListener("focus", Ripples.start);
+
+// pause ripples on click
+document.querySelector(".play-pause").addEventListener("click", Ripples.toggle);
+
+//
+let pausePlayContainer = document.querySelector(".play-pause");
+let pausePlayBtn = document.querySelectorAll(".play-pause > .icon");
+
+pausePlayContainer.addEventListener("click", () => {
+	pausePlayBtn.forEach((btn) => {
+		btn.classList.toggle("hide");
+	});
+});
