368de6ed74b30a6dde4c9ba48647b423323205f4
Mohsin Khan <muhammadmohsinkhan.aptech@gmail.com>
2024-04-22 14:32:16 +0500

    settings change added

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-let winHeight, winWidth, running, paused, mousePosX, mousePosY;
+let winHeight,
+	winWidth,
+	running,
+	paused,
+	mousePosX,
+	mousePosY,
+	rippleClass = "ripple-new",
+	randomAnimDuration = false;
+//
 
 // get  window width and height
 const resizeObserver = new ResizeObserver((entries) => {
@@ -8,17 +16,15 @@ const resizeObserver = new ResizeObserver((entries) => {
 
 resizeObserver.observe(document.body);
 
-// get mouse location
-document.onmousemove = function (e) {
+// get mouse/touch/pointer location
+document.addEventListener("pointermove", (e) => {
 	mousePosX = e.clientX;
 	mousePosY = e.clientY;
 	// console.log(mousePosX + " " + mousePosY);
-};
+});
 
 // get random number
 function getRandomInt(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
@@ -37,6 +43,7 @@ function setRangedInterval(intervalFunction, minDelay, maxDelay) {
 		} else {
 			delay = getRandomInt(minDelay, maxDelay);
 		}
+
 		timeout = setTimeout(timeoutFunction, delay);
 	};
 
@@ -55,21 +62,24 @@ let rippleContainer = document.querySelector(".ripple-container");
 
 function placeRippleNode() {
 	let o1 = document.createElement("div");
-	o1.classList.add("ripple");
+	o1.classList.add(rippleClass);
 
 	let x = getRandomInt(0, winWidth);
 	let y = getRandomInt(0, winHeight);
 
 	o1.style.left = x + "px";
 	o1.style.top = y + "px";
-	o1.style.animationDuration = getRandomInt(1500, 3000) + "ms";
+
+	if (randomAnimDuration === true) {
+		o1.style.animationDuration = getRandomInt(2500, 5000) + "ms";
+	}
 
 	rippleContainer.appendChild(o1);
 }
 
 // remove extra ripple objects from dom so its not cluttered
 function removeRippleNode() {
-	if (rippleContainer.childElementCount > 10) {
+	if (rippleContainer.childElementCount > 5) {
 		rippleContainer.removeChild(rippleContainer.firstChild);
 	}
 }
@@ -81,7 +91,7 @@ running = true;
 paused = false;
 
 // declare ripple remove function
-const removeRipple = setRangedInterval(removeRippleNode, 500, 1500);
+const removeRipple = setRangedInterval(removeRippleNode, 500, 500);
 removeRipple.start();
 
 // getting play-pause buttons and their container
@@ -160,11 +170,14 @@ let dragged = 0;
 // function to place ripples where mouse is
 function placeRippleNodeOnMouse() {
 	let o1 = document.createElement("div");
-	o1.classList.add("ripple");
+	o1.classList.add(rippleClass);
 
 	o1.style.left = mousePosX + "px";
 	o1.style.top = mousePosY + "px";
-	o1.style.animationDuration = getRandomInt(1000, 3000) + "ms";
+
+	if (randomAnimDuration === true) {
+		o1.style.animationDuration = getRandomInt(2500, 5000) + "ms";
+	}
 
 	rippleContainer.appendChild(o1);
 }
@@ -189,7 +202,7 @@ rippleContainer.addEventListener("mousemove", () => {
 	}
 });
 // on mouse up stop mouse ripples
-rippleContainer.addEventListener("mouseup", () => {
+document.addEventListener("mouseup", () => {
 	placeRippleNodeOnDrag.stop();
 	dragged = 0;
 });
@@ -202,11 +215,10 @@ let volumeBtn = document.querySelectorAll(".volume-control > .icon");
 const volumeIconChange = arrayClassNameToggle(volumeBtn, "hide");
 
 // declare audio
-const myAudio = new Audio(
-	"audio/After Rain — Zackross  Background Music  Audio Library Release.mp3"
-);
+const myAudio = new Audio("audio/After Rain — Zackross.mp3");
 
 // play audio
+myAudio.loop = true;
 myAudio.play();
 // set audio volume
 myAudio.volume = 0.05;
@@ -216,8 +228,10 @@ volumeContainer.addEventListener("click", () => {
 	volumeIconChange();
 	if (myAudio.paused === false) {
 		myAudio.pause();
+		musicEnableSetting.checked = false;
 	} else {
 		myAudio.play();
+		musicEnableSetting.checked = true;
 	}
 });
 
@@ -235,15 +249,89 @@ function optionsMenuToggle() {
 cogwheelBtn.addEventListener("click", optionsMenuToggle);
 optionCloseBtn.addEventListener("click", optionsMenuToggle);
 
+// getting menu headers
 let settingsHeader = document.querySelector(".settings-header");
 let audioHeader = document.querySelector(".audio-header");
+let optionMenus = document.querySelectorAll(".options-container > *");
 
+// when clicked on settings header toggle .option-selected class
 settingsHeader.addEventListener("click", () => {
 	settingsHeader.classList.add("option-selected");
 	audioHeader.classList.remove("option-selected");
+	optionMenus.forEach((optionMenu) => {
+		optionMenu.style.translate = "0%";
+	});
 });
 
+// when clicked on audio header toggle .option-selected class
 audioHeader.addEventListener("click", () => {
 	audioHeader.classList.add("option-selected");
 	settingsHeader.classList.remove("option-selected");
+	optionMenus.forEach((optionMenu) => {
+		optionMenu.style.translate = "-100%";
+	});
+});
+
+// -- general settings
+
+// setting to change ripple animation style ( new or old )
+let rippleStyleSetting = document.querySelector("#ripple-style");
+
+rippleStyleSetting.addEventListener("change", () => {
+	let style = rippleStyleSetting.value;
+
+	if (style === "new") {
+		rippleClass = "ripple-new";
+	} else {
+		rippleClass = "ripple-old";
+	}
+});
+
+// setting to stop/start random ripple animation duration
+let rippleAnimationDuration = document.querySelector("#anim-duration");
+
+rippleAnimationDuration.addEventListener("change", () => {
+	let checked = rippleAnimationDuration.checked;
+
+	if (checked === true) {
+		randomAnimDuration = true;
+	} else {
+		randomAnimDuration = false;
+	}
+});
+
+// -- audio settings
+
+// enable/disable music setting
+let musicEnableSetting = document.querySelector("#music-play");
+
+// check if audio is playing then change icon
+if (myAudio.paused === true) {
+	volumeIconChange();
+	musicEnableSetting.checked = false;
+} else {
+	musicEnableSetting.checked = true;
+}
+
+musicEnableSetting.addEventListener("change", () => {
+	let checked = musicEnableSetting.checked;
+
+	if (checked === true) {
+		myAudio.play();
+		volumeIconChange();
+	} else {
+		myAudio.pause();
+		volumeIconChange();
+	}
+});
+
+// music volume setting
+let volumeSetting = document.querySelectorAll("input[name=music-volume]");
+
+volumeSetting.forEach((x) => {
+	x.addEventListener("change", () => {
+		volumeSetting[0].value = volumeSetting[1].value = x.value;
+		let volume = x.value / 100;
+		myAudio.volume = volume;
+	});
 });
